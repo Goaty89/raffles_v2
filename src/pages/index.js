7b32c984284f2f1ee280f931ac0e6612d25d869f
Yong Yau <charles@Yongs-MacBook-Pro.local>
2021-10-31 22:49:54 +0800

    Add content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,124 @@ import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./index.module.css";
 import Footer from "../components/footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
+const services = [
+  {
+    title: "Fund Formation & Structuring",
+    content: (
+      <>
+        <h5>Assistance and advice on fund structure and setup</h5>
+        <h5>Review of fund documentation, agreements and procedures</h5>
+        <h5>
+          Liaison with investors and assistance with anti-money laundering
+          requirements
+        </h5>
+        <h5>Formation of required entities in Asia</h5>
+        <h5>Assistance with appointment of non-executive directors</h5>
+      </>
+    ),
+  },
+  {
+    title: "Fund Accounting & Financing Reporting",
+    content: (
+      <>
+        <h5 className={styles.wieghtMedium}>
+          Bookkeeping & maintenance of accounting records
+        </h5>
+        <h5>Coordination with auditors on statutory audit</h5>
+        <h5>Distribution calculations</h5>
+        <h5>Calculation of carried interest</h5>
+        <h5>
+          Preparation of monthly, quarterly and annual unaudited fund accounts
+        </h5>
+        <h5>Capital draw down calculations</h5>
+        <h5>Local tax filings</h5>
+      </>
+    ),
+  },
+  {
+    title: "Fund Secretarial & Compliance",
+    content: (
+      <>
+        <h5>Provision of company secretary</h5>
+        <h5>Provision of registered office</h5>
+        <h5>Filing of annual returns</h5>
+        <h5>Maintenance of statutory registers</h5>
+        <h5>Maintenance of minute books and statutory returns</h5>
+        <h5>Other statutory compliance</h5>
+      </>
+    ),
+  },
+  {
+    title: "Fund Investor Relations",
+    content: (
+      <>
+        <h5>Maintenance of Investor Registers</h5>
+        <h5>Issue of capital call and distribution notices</h5>
+        <h5>Distribution of quarterly investor reports</h5>
+        <h5>Preparation and distribution of capital statements</h5>
+        <h5>Attending to investor queries and requests</h5>
+      </>
+    ),
+  },
+  {
+    title: "Fund Cash Flow Monitoring & Reporting",
+    content: (
+      <>
+        <h5>Monitoring of bank accounts</h5>
+        <h5>Processing of capital calls and distributions</h5>
+        <h5>Processing of fund expenses</h5>
+        <h5>Performance of bank reconciliations</h5>
+        <h5>Provision of authorized bank signatories</h5>
+      </>
+    ),
+  },
+  {
+    title: "And More",
+    content: (
+      <>
+        <h5>Payroll processing</h5>
+        <h5>Assistance with work permit application</h5>
+      </>
+    ),
+  },
+];
+
+const renderServices = () => {
+  return (
+    <div className={styles.services}>
+      {services.map((service, index) => (
+        <div className={styles.row} key={`service_${index}`}>
+          <div>
+            <h4 className={styles.serviceTitle}>{service.title}</h4>
+            <div className={styles.serviceContent}>{service.content}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 const IndexPage = () => {
   return (
     <div>
       <section className={styles.hero}>
-        <div className={styles.logo}>Logo</div>
+        <div className={styles.logo}>
+          <StaticImage
+            className={styles.imgLogo}
+            alt="raffles"
+            src="../images/logo.png"
+          />
+        </div>
         <div>
-          <h1>Raffles Accounting</h1>
+          <h1>Raffles Fund Services</h1>
           <h5>Expertise. Commitment. Value.</h5>
         </div>
       </section>
-      <section className={styles.sectionCompanyProfile}>
+      <section
+        className={`${styles.sectionCompanyProfile} ${styles.limitScreenSize}`}
+      >
         <div>
           <StaticImage
             className={styles.imgCompany}
@@ -23,7 +129,9 @@ const IndexPage = () => {
           />
         </div>
         <div className={styles.companyDetails}>
-          <h2 className={styles.wieghtMedium}>Company profile</h2>
+          <h2 className={styles.wieghtMedium}>
+            Raffles Fund Services Pte. Ltd.
+          </h2>
           <h5 className={styles.wieghtMedium}>
             Established in 1992, Stratford Accounting is a full-service firm
             offering affordable accounting solutions to individuals and local
@@ -34,7 +142,7 @@ const IndexPage = () => {
           </button>
         </div>
       </section>
-      <section className={styles.sectionClient}>
+      {/* <section className={styles.sectionClient}>
         <div className={styles.titleClient}>
           <h3>Our Client Roster</h3>
           <h5 className={styles.wieghtMedium}>
@@ -61,8 +169,10 @@ const IndexPage = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section className={styles.sectionOurService}>
+      </section> */}
+      <section
+        className={`${styles.sectionOurService} ${styles.limitScreenSize}`}
+      >
         <div className={styles.titleService}>
           <h2>Our Services</h2>
           <div>
@@ -73,37 +183,36 @@ const IndexPage = () => {
             />
           </div>
         </div>
-        <div className={styles.services}>
-          <div className={styles.row}>
-            <span>01</span>
-            <div>
-              <h4>Tax Preparation</h4>
-              <h5 className={styles.wieghtMedium}>
-                We prepare everything you need for your tax return
-              </h5>
-            </div>
-          </div>
-          <div className={styles.row}>
-            <span>02</span>
-            <div>
-              <h4>Start-up Package</h4>
-              <h5 className={styles.wieghtMedium}>
-                We build financial models you can pitch to investors
-              </h5>
-            </div>
-          </div>
-          <div className={styles.row}>
-            <span>03</span>
-            <div>
-              <h4>Wealth Management</h4>
-              <h5 className={styles.wieghtMedium}>
-                We deliver thoughtful investment advisory services
-              </h5>
-            </div>
-          </div>
+        <div>
+          <p>
+            Investments using hard-earned money require a highly experienced and
+            trusted fund manager, whereby your investments will earn your
+            aspired returns as well as secured and hassle-free. We strongly
+            believe in creating and nurturing a long-term relationship with our
+            clients based on mutual trust and a win-win outcome. Towards this
+            end, we are committed to maintaining regular communication with
+            regards to providing sound advice on market trends and performance
+            so that our clients will be able to make informed decisions on their
+            investments.
+          </p>
+          <br />
+          <p>
+            To complement our market intelligence and advice, we have a team of
+            qualified accounting personnel who will present your investment
+            portfolios and performance in accordance with the market benchmark
+            on accounting practices. Every task entrusted to us will be managed
+            and reported with the highest level of professionalism and
+            transparency. Our company offers a full range of investments,
+            company incorporation and secretarial services, taxation and
+            accounting services, as follows:
+          </p>
         </div>
+        <div className={styles.servicesQuestion}>
+          <h3>What does Raffles offer?</h3>
+        </div>
+        {renderServices()}
       </section>
-      <section className={styles.sectionCareer}>
+      <section className={`${styles.sectionCareer} ${styles.limitScreenSize}`}>
         <div className={styles.titleCareer}>
           <h3>Let's work together.</h3>
           <div>
@@ -117,15 +226,15 @@ const IndexPage = () => {
         <div className={styles.careerInfo}>
           <div className={styles.row}>
             <h4>Mailing Address</h4>
-            <h5>123 Anywhere Road. Any City, CT 12345</h5>
+            <h5>2 Havelock Road, #05-22 Havelock II, Singapore 059763</h5>
           </div>
           <div className={styles.row}>
             <h4>Phone Number</h4>
-            <h5>(010) 774-868123</h5>
+            <h5>+65 6221 4030</h5>
           </div>
           <div className={styles.row}>
             <h4>Email Address</h4>
-            <h5>hello@samplesite.com</h5>
+            <h5>enquiry@rafflesfundservices.com</h5>
           </div>
         </div>
       </section>
